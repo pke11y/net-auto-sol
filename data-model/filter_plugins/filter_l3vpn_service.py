e5d2c19997b321f513366e2835a3fcc39e812f5d
pk <pk@netmngr.pk.lab>
2020-05-24 17:13:08 +0100

    Submitting Exercise 4

--- data-model/filter_plugins/filter_l3vpn_service.py
+++ data-model/filter_plugins/filter_l3vpn_service.py
@@ -23,24 +23,27 @@ def get_service_test_ip(services):
                     service_test_ip[vrf_name] = dest_ip_list                          
     return service_test_ip
 
-def find_vrfs_with_ip(parsed_vpnv4_routes, ip_prefix, allowed_vrfs=[]):
-    """ Get all of the VRF's that have the IP address in the VRF route table 
+def verify_vpnv4_status(parsed_vpnv4_routes, ip_prefix, allowed_vrfs=[]):
+    """ Check to see if another VRF has the IP address in the VRF route table 
         
-        Return a list of VRF names that have a routing table entry for the 
-        List: [vrf_name1, vrf_name2,..]
+        Return: true/false if find another a route in another VRF that is not in the 'allowed' list
+
+        TODO: rather than just true/false
+            Return a list of VRF names that have a routing table entry for the 
+            List: [vrf_name1, vrf_name2,..]
     """
     all_vrfs = parsed_vpnv4_routes['vrf']
-    active_vrfs = []
+    # active_vrfs = []
     prefix = IPv4Network(ip_prefix)
-    for vrf, vrf_data in all_vrfs.items():
+    for vrf_name, vrf_data in all_vrfs.items():
+        if vrf_name in allowed_vrfs:
+            continue
         for vpnv4_type, vpnv4_data in vrf_data['address_family'].items():
-            vrf_name = vpnv4_data['default_vrf']
-            if vrf_name in allowed_vrfs:
-                continue
-            for ip_pfx, ip_pfx_data in vpnv4_data['routes'].items():
+            # vrf_name = vpnv4_data['default_vrf']
+            for ip_pfx in vpnv4_data['routes']:
                 if IPv4Network(ip_pfx) == prefix:
-                    active_vrfs.append(vrf_name)
-    return active_vrfs
+                    return False
+    return True
 
 def get_cpe_allowed_vrfs(cpenodes):
     """ Return a dict of CPE routers with a list of remote VRF names they connect to
@@ -69,6 +72,6 @@ def parse_xr_vrf(stdout_lines):
 class FilterModule(object):
     def filters(self):
         return {"get_service_test_ip": get_service_test_ip,
-                "find_vrfs_with_ip": find_vrfs_with_ip,
+                "verify_vpnv4_status": verify_vpnv4_status,
                 "get_cpe_allowed_vrfs": get_cpe_allowed_vrfs,
                 "parse_xr_vrf": parse_xr_vrf}
